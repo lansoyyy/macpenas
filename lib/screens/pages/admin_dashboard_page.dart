f8c76427b5610fb6b6cc4eb6f703e5ae2d349185
lansoyyy <olanalans12345@gmail.com>
2023-10-31 11:32:19 +0800

    changed to gmap

--- lib/screens/pages/admin_dashboard_page.dart
+++ lib/screens/pages/admin_dashboard_page.dart
@@ -1,12 +1,16 @@
 import 'dart:async';
+import 'dart:collection';
 import 'package:badges/badges.dart' as b;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
+
 import 'package:geolocator/geolocator.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
 import 'package:intl/intl.dart';
-import 'package:latlong2/latlong.dart';
+
 import '../../widgets/text_widget.dart';
 
 class AdminDashboardScreen extends StatefulWidget {
@@ -21,7 +25,7 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
   double long = 0;
 
   bool hasLoaded = false;
-  final mapController = MapController();
+ 
 
   final box = GetStorage();
 
@@ -34,15 +38,15 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
         lat = position.latitude;
         long = position.longitude;
         hasLoaded = true;
-        myCircles.add(
-          CircleMarker(
-              point: LatLng(position.latitude, position.longitude),
-              radius: 5,
-              borderStrokeWidth: 1,
-              borderColor: Colors.black,
-              useRadiusInMeter: true,
-              color: Colors.blue),
-        );
+        // myCircles.add(
+        //   CircleMarker(
+        //       point: LatLng(position.latitude, position.longitude),
+        //       radius: 5,
+        //       borderStrokeWidth: 1,
+        //       borderColor: Colors.black,
+        //       useRadiusInMeter: true,
+        //       color: Colors.blue),
+        // );
       });
       // getDirections(LatLng(position.latitude, position.longitude),
       //     LatLng(8.306295, 124.992959));
@@ -53,44 +57,57 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
     update();
   }
 
-  late List<CircleMarker> myCircles = [];
-  late List<Polyline> myPoly = [];
-  late List<Marker> myMarker = [];
+  // late List<CircleMarker> myCircles = [];
+ 
 
+
+ GoogleMapController? mapController;
+
+  final latController = TextEditingController();
+  final longController = TextEditingController();
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  
+
+  String newUrl = '';
+  Set<Polygon> polygon = HashSet<Polygon>();
   update() async {
     Timer.periodic(const Duration(seconds: 10), (timer) {
       Geolocator.getCurrentPosition().then((position) {
-        setState(() {
-          myPoly.clear();
-          myCircles.clear();
+        // setState(() {
+        //   myPoly.clear();
+        //   myCircles.clear();
 
-          myCircles.add(
-            CircleMarker(
-                point: LatLng(position.latitude, position.longitude),
-                radius: 5,
-                borderStrokeWidth: 1,
-                borderColor: Colors.black,
-                useRadiusInMeter: true,
-                color: Colors.blue),
-          );
+        //   myCircles.add(
+        //     CircleMarker(
+        //         point: LatLng(position.latitude, position.longitude),
+        //         radius: 5,
+        //         borderStrokeWidth: 1,
+        //         borderColor: Colors.black,
+        //         useRadiusInMeter: true,
+        //         color: Colors.blue),
+        //   );
 
-          // mapController.move(LatLng(position.latitude, position.longitude), 18);
+        //   // mapController.move(LatLng(position.latitude, position.longitude), 18);
 
-          lat = position.latitude;
-          long = position.longitude;
-        });
-        FirebaseFirestore.instance
-            .collection('Reports')
-            .where('status', isEqualTo: 'Pending')
-            .get()
-            .then((QuerySnapshot querySnapshot) async {
-          for (var doc in querySnapshot.docs) {
-            myPoly.add(Polyline(points: [
-              LatLng(position.latitude, position.longitude),
-              LatLng(doc['lat'], doc['long']),
-            ]));
-          }
-        });
+        //   lat = position.latitude;
+        //   long = position.longitude;
+        // });
+        // FirebaseFirestore.instance
+        //     .collection('Reports')
+        //     .where('status', isEqualTo: 'Pending')
+        //     .get()
+        //     .then((QuerySnapshot querySnapshot) async {
+        //   for (var doc in querySnapshot.docs) {
+        //     myPoly.add(Polyline(points: [
+        //       LatLng(position.latitude, position.longitude),
+        //       LatLng(doc['lat'], doc['long']),
+        //     ]));
+        //   }
+        // });
       }).catchError((error) {
         print('Error getting location: $error');
       });
@@ -102,6 +119,12 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
     double screenWidth = MediaQuery.of(context).size.width;
 
     bool isLargeScreen = screenWidth >= 600;
+
+
+       CameraPosition initialCameraPosition =  CameraPosition(
+      target: LatLng(8.1479, 125.1321),
+      zoom: 14,
+    );
 
     return hasLoaded
         ? Container(
@@ -281,139 +304,15 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
                       return Expanded(
                         child: SizedBox(
                           height: 425,
-                          child: FlutterMap(
-                            mapController: mapController,
-                            options: MapOptions(
-                              center: LatLng(lat, long),
-                              zoom: 18.0,
-                            ),
-                            children: [
-                              TileLayer(
-                                urlTemplate:
-                                    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                                userAgentPackageName: 'com.example.app',
-                              ),
-                              MarkerLayer(
-                                markers: [
-                                  for (int i = 0; i < data.docs.length; i++)
-                                    Marker(
-                                      height: 150,
-                                      width: 50,
-                                      point: LatLng(data.docs[i]['lat'],
-                                          data.docs[i]['long']),
-                                      builder: (context) {
-                                        return SizedBox(
-                                          width: 300,
-                                          height: 150,
-                                          child: Row(
-                                            children: [
-                                              const SizedBox(),
-                                              Align(
-                                                alignment: Alignment.topRight,
-                                                child: Container(
-                                                  width: 300,
-                                                  height: 150,
-                                                  decoration: BoxDecoration(
-                                                      color: Colors.white60,
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              10)),
-                                                  child: Padding(
-                                                    padding: const EdgeInsets
-                                                            .fromLTRB(
-                                                        20, 10, 20, 10),
-                                                    child: Column(
-                                                      crossAxisAlignment:
-                                                          CrossAxisAlignment
-                                                              .start,
-                                                      mainAxisAlignment:
-                                                          MainAxisAlignment
-                                                              .center,
-                                                      children: [
-                                                        Row(
-                                                          mainAxisAlignment:
-                                                              MainAxisAlignment
-                                                                  .spaceBetween,
-                                                          children: [
-                                                            TextBold(
-                                                              text: data.docs[i]
-                                                                  ['name'],
-                                                              fontSize: 16,
-                                                              color:
-                                                                  Colors.black,
-                                                            ),
-                                                          ],
-                                                        ),
-                                                        const SizedBox(
-                                                          height: 5,
-                                                        ),
-                                                        TextRegular(
-                                                          text: data.docs[i]
-                                                              ['contactNumber'],
-                                                          fontSize: 12,
-                                                          color: Colors.grey,
-                                                        ),
-                                                        const SizedBox(
-                                                          height: 2.5,
-                                                        ),
-                                                        TextRegular(
-                                                          text: data.docs[i]
-                                                              ['address'],
-                                                          fontSize: 12,
-                                                          color: Colors.grey,
-                                                        ),
-                                                        const SizedBox(
-                                                          height: 5,
-                                                        ),
-                                                        TextBold(
-                                                          text: data.docs[i]
-                                                              ['type'],
-                                                          fontSize: 14,
-                                                          color: Colors.black,
-                                                        ),
-                                                        const SizedBox(
-                                                          height: 5,
-                                                        ),
-                                                        TextBold(
-                                                          text:
-                                                              'Reporter is a ${data.docs[i]['reporterType']}',
-                                                          fontSize: 14,
-                                                          color: Colors.black,
-                                                        ),
-                                                        const SizedBox(
-                                                          height: 2.5,
-                                                        ),
-                                                        TextRegular(
-                                                          text: DateFormat
-                                                                  .yMMMd()
-                                                              .add_jm()
-                                                              .format(data
-                                                                  .docs[i][
-                                                                      'dateTime']
-                                                                  .toDate()),
-                                                          fontSize: 12,
-                                                          color: Colors.grey,
-                                                        ),
-                                                      ],
-                                                    ),
-                                                  ),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        );
-                                      },
-                                    )
-                                ],
-                              ),
-                              PolylineLayer(
-                                polylines: myPoly,
-                              ),
-                              CircleLayer(
-                                circles: myCircles,
-                              ),
-                            ],
-                          ),
+                          
+                          child: GoogleMap(
+                            myLocationEnabled: true,
+                    zoomControlsEnabled: false,
+                    mapType: MapType.normal,
+                    polygons: polygon,
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: initialCameraPosition,
+                  ),
                         ),
                       );
                     }),
@@ -589,13 +488,14 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
                                               return Card(
                                                 child: ListTile(
                                                   onTap: () {
-                                                    mapController.move(
-                                                        LatLng(
-                                                            data.docs[index]
-                                                                ['lat'],
-                                                            data.docs[index]
-                                                                ['long']),
-                                                        18);
+                                                   mapController!.animateCamera(
+                                                    
+                                                    CameraUpdate.newCameraPosition(CameraPosition(
+                                                      
+                                                      zoom: 14,
+                                                      target: LatLng(data.docs[index]
+                                                          ['lat'], data.docs[index]
+                                                          ['long']))));
                                                   },
                                                   leading: isLargeScreen
                                                       ? Image.asset(
